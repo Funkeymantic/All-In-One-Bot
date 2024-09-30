6d1fe21c919657ef6fb247a9929213e632e535f6
Funkeymantic <39865124+Funkeymantic@users.noreply.github.com>
2024-09-30 16:11:26 -0700

    Update twitch_bot.py

--- bot_modules/twitch_bot.py
+++ bot_modules/twitch_bot.py
@@ -34,8 +34,33 @@ class TwitchBot(twitch_commands.Bot):
                 await message.channel.send(f'I claim your CHEESE, {message.author.name}!')
                 self.last_cheese_response_time = current_time
 
+    @twitch_commands.command(name='hello')
+    async def hello(self, ctx: twitch_commands.Context):
+        await ctx.send(f'Hello {ctx.author.name}!')
+
+    async def send_twitch_message(self, content):
+        channel = self.get_channel('funkeymantic')
+        if channel:
+            await channel.send(content)
+
+# Function to check if the Twitch stream is live
+async def check_twitch_live():
+    TWITCH_USER_LOGIN = 'funkeymantic'
+    headers = {
+        'Client-ID': os.getenv("TWITCH_CLIENT_ID"),
+        'Authorization': f'Bearer {os.getenv("TWITCH_TOKEN")}'
+    }
+    async with aiohttp.ClientSession() as session:
+        url = f'https://api.twitch.tv/helix/streams?user_login={TWITCH_USER_LOGIN}'
+        async with session.get(url, headers=headers) as resp:
+            if resp.status == 200:
+                data = await resp.json()
+                if 'data' in data and len(data['data']) > 0:
+                    thumbnail_url = data['data'][0]['thumbnail_url'].replace("{width}", "320").replace("{height}", "180")
+                    return True, thumbnail_url
+            return False, None
+
 # Function to start the Twitch bot
 async def start_twitch_bot():
     twitch_bot = TwitchBot()
     await twitch_bot.start()
-
